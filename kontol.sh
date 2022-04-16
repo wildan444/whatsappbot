9b99b0cfcc255325b37bb20285c31ced7907201d
wildan444 <62401430+wildan444@users.noreply.github.com>
2022-04-17 00:46:19 +0700

    Add files via upload

---
+++ kontol.sh
@@ -0,0 +1,8 @@
+apt install git -y
+apt install python3 -y
+apt install pip -y
+apt install aapt -y
+apt install openjdk-17 -y
+git clone https://github.com/karma9874/AndroRAT.git
+cd AndroRAT
+pip install -r requirements.txt
